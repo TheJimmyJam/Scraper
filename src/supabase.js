20a627947a936dbddd6d8b9cd23115ac9a0b5bbb
TheJimmyJam <wcannon83@gmail.com>
2026-04-21 03:23:22 -0500

    Add files via upload

---
+++ src/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl  = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey  = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
+export const LOCAL_API = import.meta.env.VITE_LOCAL_API_URL || 'http://localhost:8000'
